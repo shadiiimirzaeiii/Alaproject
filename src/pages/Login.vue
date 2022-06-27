68333ff3dc5c37c9ba8c8ff105d4fd0d869532a1
shadimirzaei <shadiii.mirzaiii.com>
2022-06-27 10:22:05 +0330

    added new user

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="row" style="height: 90vh;margin-left:500px;margin-top:10px;text-align:right">
-    <div
-         class="col-12 col-md-6 flex content-center">
-      <q-card >
+  <div
+    class="row"
+    style="
+      height: 90vh;
+      margin-left: 500px;
+      margin-top: 10px;
+      text-align: right;
+    "
+  >
+    <div class="col-12 col-md-6 flex content-center">
+      <q-card>
         <q-card-section>
-          <q-form  class="q-gutter-md" @submit.prevent="submitForm">
-            <q-input  label="شماره همراه" v-model="login.phoneNumber">
-            </q-input>
+          <q-form class="q-gutter-md" @submit.prevent="submitForm">
+            <q-input label="شماره همراه" v-model="login.phoneNumber"> </q-input>
             <q-input label="کدملی" type="password" v-model="login.nationalCode">
             </q-input>
             <div>
-              <q-btn class="full-width" color="primary" label="ورود" type="submit" @click="submitLoginForm()" rounded></q-btn>
-              <div class="text-center q-mt-sm q-gutter-lg">
-              </div>
+              <q-btn
+                class="full-width"
+                color="primary"
+                label="ورود"
+                type="submit"
+                @click="submitLoginForm()"
+                rounded
+              ></q-btn>
+              <div class="text-center q-mt-sm q-gutter-lg"></div>
             </div>
           </q-form>
         </q-card-section>
@@ -22,7 +34,6 @@
 </template>
 
 <script>
-import { useQuasar } from 'quasar'
 import { mapActions } from 'vuex'
 
 let $q
@@ -40,20 +51,29 @@ export default {
   methods: {
     ...mapActions('auth', ['doLogin']),
     async submitForm () {
-      if (!this.login.username || !this.login.password) {
+      if (!this.login.phoneNumber || !this.login.nationalCode) {
         $q.notify({
 
           message: 'نام کاربری یا پسورد اشتباه است'
         })
-      }
-    },
+      } else if(!this.login.username && this.login.password) {
+        this.addnewuser()}
 
-  },submitLoginForm: function(){
+    },
+    addnewuser:function(username,password) {
+     axios.post('alaatv.com/api/v2/definitions/UserUpdate'),
+	{
+		phoneNumber: 'phoneNumber',
+		nationalCode: 'nationalCode'
+	}
+
+
+  },
+  submitLoginForm: function(){
     this.$router.push('/profile')
   },
-  mounted () {
+  },
 
-  }
 }
 </script>
 
